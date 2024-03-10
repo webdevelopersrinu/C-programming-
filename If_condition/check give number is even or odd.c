3a24ace2592241fb9cff3c7c0aac7fa6aee5e663
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-10 10:21:39 +0530

    Create check give number is even or odd.c

---
+++ If_condition/check give number is even or odd.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main (){
+    int num=9;
+    num%=2;
+    if(num==0){
+        printf("give number is even");
+    }
+    if(num==1){
+        printf("give number is odd");
+    }
+    return 0 ;
+}
