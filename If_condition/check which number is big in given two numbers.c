5ddcf7e7d1e896305f1ed784a8a8e55ac5cb505f
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-10 10:22:45 +0530

    Create check which number is big in given two numbers.c

---
+++ If_condition/check which number is big in given two numbers.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main (){
+    int a=97;
+    int b=89;
+    if(a>b){
+        printf("a is the biggest number");
+    }
+    if(b>a){
+        printf("b is the biggest number");
+    }
+    if(a==b){
+        printf("a and b both have same values");
+    }
+    return 0 ;
+}
