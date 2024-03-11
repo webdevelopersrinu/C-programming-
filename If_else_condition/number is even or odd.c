aad264e41bfa6377a292a4fabc08fbc2ed768f80
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-11 10:41:59 +0530

    Create number is even or odd.c

---
+++ If_else_condition/number is even or odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    int a=4;
+    a %= 2;
+    if(a==0){
+    printf("give number is even");
+    } else {
+    printf("give number is odd");
+    }
+    return 0 ;
+}
