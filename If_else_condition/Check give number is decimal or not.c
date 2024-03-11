a120d938edc85447dca3d7d345d3fa06b84825e8
webdevelopersrinu <159899608+webdevelopersrinu@users.noreply.github.com>
2024-03-11 10:41:10 +0530

    Create Check give number is decimal or not.c

---
+++ If_else_condition/Check give number is decimal or not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    float a=7.7;
+    float b=1;
+    float c=fmod(a,b);
+    if (c == 0){
+        printf("give number is not decimal");
+    }else{
+        printf("give number is decimal number");
+    }
+    return 0 ;
+}
